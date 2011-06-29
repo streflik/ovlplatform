ee2a2ff8162459f9e4ed91f49351dcc1a5d6d84b
Bartek <bartek@ubuntu.ubuntu-domain>
2011-06-29 21:11:08 +0200

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ovlplatform::Application.config.secret_token = 'c6ee7272b2a7ffd382ba85c8c462f8d8114fcfc396de55244f23d74dda6323d25c3b4a2552043723dcf2637dde41c0335d908a5eac09c6f18393e99067bf8465'
